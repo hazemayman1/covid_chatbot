f8d184633d4479dc1ff1bb8060555320d68eecc6
Hazem <7azemayman@gmail.com>
2021-06-17 16:17:41 +0200

    initial commit for the application

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,44 @@
+import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flash_chat/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static const String id = "welcome_screen";
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+    animation = ColorTween(begin: Color(0x323031), end: kPrimaryColor)
+        .animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -18,57 +47,73 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                      child: Image.asset('images/image.png'),
+                      height: 60.0,
+                      padding: EdgeInsets.all(5.0),
+                      margin: const EdgeInsets.only(
+                        right: 5.0,
+                      )),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                  ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'DiCorona..?',
+                      textStyle: const TextStyle(
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w300,
+                        color: kSecondaryColor,
+                      ),
+                      speed: const Duration(milliseconds: 200),
+                    ),
+                  ],
+                  totalRepeatCount: 1,
                 ),
               ],
             ),
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            // RoundedButton(
+            //   title: 'Log in',
+            //   color: Colors.green,
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, LoginScreen.id);
+            //   },
+            // ),
+            // RoundedButton(
+            //   title: 'Register',
+            //   color: Colors.blueAccent,
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, RegistrationScreen.id);
+            //   },
+            // ),
+            RoundedButton(
+              title: 'lab results analysis'.toUpperCase(),
+              color: kPrimaryColor,
+              onPressed: () {
+                //currently just navigates to login screen
+                //to be implemented lated
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            RoundedButton(
+              title: 'chat with our bot'.toUpperCase(),
+              color: kPrimaryColor,
+              onPressed: () {
+                Navigator.pushNamed(context, ChatScreen.id);
+              },
+            ),
+            RoundedButton(
+              title: 'Chest image scan'.toUpperCase(),
+              color: kPrimaryColor,
+              onPressed: () {
+                //currently just navigates to registration screen
+                //to be implemented lated
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
             ),
           ],
         ),
